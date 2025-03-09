8731853d37e44f575d6f36dad1739afcdfc11142
Bnbldk <80970256+Bnbldk@users.noreply.github.com>
2025-03-09 04:05:38 +0000

    Deploying to gh-pages from @ Bnbldk/invite-bjj@0146e1b400196450bd17c7bc78bf40c3654ed6e3 🚀

--- firebase.js
+++ firebase.js
@@ -2,22 +2,22 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebas
 import { getDatabase, ref, onValue, push, runTransaction } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js";
 
 let firebaseConfig;
+    firebaseConfig = {
+        apiKey: "AIzaSyDD4p1KU_EcVJve8cGe6ufMllwu-sonCPg",
+        authDomain: "bruno-invite-bjj.firebaseapp.com",
+        databaseURL: "https://bruno-invite-bjj-default-rtdb.firebaseio.com/",
+        projectId: "bruno-invite-bjj",
+        storageBucket: "bruno-invite-bjj.appspot.com",
+        messagingSenderId: "405981974320",
+        appId: "1:405981974320:web:78d5f019db2631ada702f5",
+        measurementId: "G-JQPVJJECVT"
+        };
 
-        firebaseConfig = {
-            apiKey: "AIzaSyDD4p1KU_EcVJve8cGe6ufMllwu-sonCPg",
-            authDomain: "bruno-invite-bjj.firebaseapp.com",
-            databaseURL: "https://bruno-invite-bjj-default-rtdb.firebaseio.com/",
-            projectId: "bruno-invite-bjj",
-            storageBucket: "bruno-invite-bjj.appspot.com",
-            messagingSenderId: "405981974320",
-            appId: "1:405981974320:web:78d5f019db2631ada702f5",
-            measurementId: "G-JQPVJJECVT"
-            };
+// Debugging: Log Firebase API key to check if it's loading correctly
+console.log("🔥 Firebase API Key Loaded 111:", firebaseConfig.apiKey);
 
 // ✅ Check if `configProd.js` still contains placeholders (Secrets not injected)
-if (!firebaseConfig || firebaseConfig.apiKey === "AIzaSyDD4p1KU_EcVJve8cGe6ufMllwu-sonCPg") {
-    console.warn("⚠️ Firebase keys not found in configProd.js! Trying config.js...");
-
+if (firebaseConfig.apiKey === "AIzaSyDD4p1KU_EcVJve8cGe6ufMllwu-sonCPg") {
     try {
         // ✅ Try importing `config.js` (Local Development)
         const module = await import("./config.js");
