79bd14aecbe246fd28c075ab20743d78f09b38ad
Bnbldk <80970256+Bnbldk@users.noreply.github.com>
2025-03-09 03:10:31 +0000

    Deploying to gh-pages from @ Bnbldk/invite-bjj@408d917b86167f706eb5ab071ccf30992f63615c 🚀

--- firebase.js
+++ firebase.js
@@ -1,6 +1,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
-import firebaseConfig from "./firebase-config.js"; // This will be generated at deployment
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
+import firebaseConfig from "./firebase-config.js";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app); // This is your Firestore database
+
 console.log("Firebase Initialized:", app);
+export { db }; // Export the database object
